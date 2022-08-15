7b3d392146e2334e28d27d8c23a9ed885a04522b
MahanRahmati <mahan.rahmati@protonmail.com>
2022-08-15 23:42:03 +0430

    2.1.0

--- lib/strings.dart
+++ lib/strings.dart
@@ -3,7 +3,7 @@ import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 abstract class Strings {
   static const String appName = 'Arna Translate';
-  static const String version = '2.0.5';
+  static const String version = '2.1.0';
 }
 
 extension Localization on BuildContext {
